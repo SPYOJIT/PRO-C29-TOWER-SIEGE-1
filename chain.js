3a6a897aff3b89052efc74b81db54f4d7dfb886e
SPYOJIT <74659876+SPYOJIT@users.noreply.github.com>
2021-01-14 13:48:25 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,30 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 5,
+        }
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA=null;
+    }
+    attach(body){
+        this.sling.bodyA=body;
+    }
+    display(){
+        push();
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke(68,224,208);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        pop();
+        }
+    }
+}
